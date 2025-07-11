b51dd58c55480bb34b865c670528d09d2f106454
fogel98n <fogel98nex@gmail.com>
2025-07-11 09:12:30 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "https://juegos.proyectoscl.space";
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://juegos.proyectoscl.space";
+//export const BASE_URL = "http://localhost:5000";
